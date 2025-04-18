ef4829938b0370f24b887022e51f79e8d852691b
Quicy <owo@owodeMac-mini.local>
2025-04-18 17:49:55 +0800

    修改构建

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ const path=require("path")
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app:{
+    baseURL: '/nuxt-github-pages/'
+  },
   nitro:{
     output:{
       publicDir:path.join(__dirname,'docs')
