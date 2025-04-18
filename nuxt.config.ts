da2d5899c6c6ea8c7f759940d2b69801aea874ad
Quicy <owo@owodeMac-mini.local>
2025-04-18 17:45:49 +0800

    修改编译路径

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+const path=require("path")
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro:{
+    output:{
+      publicDir:path.join(__dirname,'docs')
+    }
+  }
 })
